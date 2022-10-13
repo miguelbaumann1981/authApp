4722ec5521ab894d4cf3d5734357649542c533bd
MiguelMartinezBBVA <miguel.martinez3.contractor@bbva.com>
2022-10-13 11:24:56 +0200

    avance lunes

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +14,22 @@ export class LoginComponent  {
 
   miFormulario: FormGroup = this.fb.group({
     email: ['test1@test.com', [Validators.required, Validators.email]],
-    password: ['123456', [Validators.required, Validators.minLength(6)]],
+    password: ['12345', [Validators.required, Validators.minLength(5)]],
   });
 
-  constructor( private fb: FormBuilder, private router: Router ) { }
+  constructor( private fb: FormBuilder, private router: Router, private authService: AuthService ) { }
 
   login() {
     console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
-    this.router.navigateByUrl('/dashboard');
+    const {email, password} = this.miFormulario.value;
+    this.authService.login(email, password).subscribe( valid => {
+      console.log(valid);
+      if (valid === true) {
+        this.router.navigateByUrl('/dashboard');
+      } else {
+        Swal.fire('Error', valid, 'error')
+      }
+    });
   }
 
  
